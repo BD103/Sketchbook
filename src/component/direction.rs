c97e1de255f60109e8f039be854916ff12498819
BD103 <59022059+BD103@users.noreply.github.com>
2022-06-11 20:50:20 -0400

    feat: add direction component

---
+++ src/component/direction.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,5 +1,7 @@
 mod position;
 mod velocity;
+mod direction;
 
 pub use self::position::Position;
 pub use self::velocity::Velocity;
+pub use self::direction::Direction;
