12ecccac87909dfcd238485c4d0d01a0673e0ad0
BD103 <59022059+BD103@users.noreply.github.com>
2022-06-13 15:57:24 -0400

    feat: configure window

--- src/plugin/main.rs
+++ src/plugin/main.rs
@@ -5,7 +5,12 @@ pub struct MainPlugin;
 
 impl Plugin for MainPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(ClearColor(palette::MONO.0))
-            .add_startup_system(system::setup);
+        app.insert_resource(WindowDescriptor {
+            title: "Sketchbook".to_string(),
+            resizable: false,
+            ..default()
+        })
+        .insert_resource(ClearColor(palette::MONO.0))
+        .add_startup_system(system::setup);
     }
 }
