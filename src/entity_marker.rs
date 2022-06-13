e45df43e0e39196e6c6bd6003e4d6a04835df090
BD103 <59022059+BD103@users.noreply.github.com>
2022-06-12 21:34:28 -0400

    feat: add entity_marker module

---
+++ src/entity_marker.rs
@@ -0,0 +1,19 @@
+//! Each unique entity type requires an entity marker like the following. This
+//! allows entity types to be query-able.
+//!
+//! Each marker should be implemented like the following:
+//!
+//! ```
+//! # use bevy::prelude::*;
+//! #
+//! #[derive(Component)]
+//! pub struct EntityName;
+//! ```
+//!
+//! Additional derives are allowed. Custom `impls` are allowed as well, but
+//! probably shouldn't be used as much.
+
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Player;
