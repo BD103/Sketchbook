26c739d9bd2f2049c102bfdd876773a26ef62515
BD103 <59022059+BD103@users.noreply.github.com>
2022-06-13 15:43:49 -0400

    feat: exit on escape
    Only for development, though!

--- src/plugin/dev.rs
+++ src/plugin/dev.rs
@@ -1,3 +1,4 @@
+use bevy::input::system::exit_on_esc_system;
 use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
 
@@ -5,6 +6,7 @@ pub struct DevPlugin;
 
 impl Plugin for DevPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(WorldInspectorPlugin::new());
+        app.add_plugin(WorldInspectorPlugin::new())
+            .add_system(exit_on_esc_system);
     }
 }
