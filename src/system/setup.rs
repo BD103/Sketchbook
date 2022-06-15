94ec7490840c47d4d5ee786d13664c8f69c20a3d
BD103 <59022059+BD103@users.noreply.github.com>
2022-06-14 20:38:25 -0400

    feat: name camera

--- src/system/setup.rs
+++ src/system/setup.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
 pub fn setup(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d()).insert(Name::new("Camera"));
     info!("Finished initializing.");
 }
