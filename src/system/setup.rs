9da760bcb1f587d974baa21c4260168788ac5729
BD103 <59022059+BD103@users.noreply.github.com>
2022-06-14 20:57:58 -0400

    feat: add ui camera

--- src/system/setup.rs
+++ src/system/setup.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 pub fn setup(mut commands: Commands) {
     commands
         .spawn_bundle(OrthographicCameraBundle::new_2d())
-        .insert(Name::new("Camera"));
+        .insert(Name::new("Main Camera"));
+    commands.spawn_bundle(UiCameraBundle::default()).insert(Name::new("UI Camera"));
     info!("Finished initializing.");
 }
